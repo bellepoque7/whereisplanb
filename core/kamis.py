c0466f9597ac04fb1554d22210dc17031166ea33
bellepoque7 <snowgot@gmail.com>
2025-01-21 12:59:27 +0900

    디렉토리정리

--- core/kamis.py
+++ core/kamis.py
@@ -9,11 +9,22 @@ def dailypricecategory(
     category_detail_code: str = '224',
     country_code: str = '',
     regday: str = '2022-12-01',
-    convert_kg_yn: str = 'N',
+    convert_kg_yn: str = 'N'
 ) -> requests.Response:
 
     """
     API 1번("일별 부류별 도.소매가격정보")의 응답 상황을 확인하기 위한 함수입니다.
+    
+    category_detail_code: 고구마 품목코드는 151이며 100번을 호출한다음에 필터링해야함
+    
+    p_cert_key	string	인증Key
+    p_cert_id	string	요청자id
+    p_returntype	string	Return Type (json:Json 데이터 형식, xml:XML데이터형식)
+    p_product_cls_code	string	구분 ( 01:소매, 02:도매, default:02 )
+    p_item_category_code	string	부류코드(100:식량작물, 200:채소류, 300:특용작물, 400:과일류, 500:축산물, 600:수산물, default:100)
+    p_country_code	string	* 소매가격 선택가능 지역 (1101:서울, 2100:부산, 2200:대구, 2300:인천, 2401:광주, 2501:대전, 2601:울산, 3111:수원, 3214:강릉, 3211:춘천, 3311:청주, 3511:전주, 3711:포항, 3911:제주, 3113:의정부, 3613:순천, 3714:안동, 3814:창원, 3145:용인, 2701:세종, 3112:성남, 3138:고양, 3411:천안, 3818:김해)<br />* 도매가격 선택가능 지역 (1101:서울, 2100:부산, 2200:대구, 2401:광주, 2501:대전)<br /> default : 전체지역
+    p_regday	string	날짜 : yyyy-mm-dd (default : 최근 조사일자)
+    p_convert_kg_yn	string	kg단위 환산여부(Y : 1kg 단위표시, N : 정보조사 단위표시, ex: 쌀 20kg)<br /> default : N
     """
     url = 'http://www.kamis.or.kr/service/price/xml.do?action=dailyPriceByCategoryList'
     params = {
@@ -21,7 +32,7 @@ def dailypricecategory(
         'p_cert_id': os.environ['kamis_api_key'],
         'p_returntype': 'json',
         'p_product_cls_code': cls_code,
-        'p_item_category_code': category_detail_code[0] + '00',
+        'p_item_category_code': category_detail_code[0],
         'p_country_code': country_code,
         'p_regday': regday,
         'p_convert_kg_yn': convert_kg_yn
